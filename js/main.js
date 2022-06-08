e5e16271a367769319bed7db4ad3fb17a520116c
LY02 <a0202denny@gmail.com>
2022-06-08 18:32:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const gameCanvas = new Game.Init();
 const rand = (n) => Math.floor(Math.random() * n)
 
 const notes = Array.from(
-    {length: 1}, 
+    {length: 8}, 
     (x, index) => new Game.note(gameCanvas.scene, index % 8, Math.floor(index / 8))
 );
 Game.gameLoop(gameCanvas, notes);
