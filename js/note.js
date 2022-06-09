57267c13d0bbcc72db1f0c35f51ffc901a8cd9bd
LY02 <a0202denny@gmail.com>
2022-06-10 00:38:32 +0800

    Update note.js

--- js/note.js
+++ js/note.js
@@ -178,14 +178,14 @@ const Note = {
 
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                text = xhr.responseText;
+                text = xhr.responseText.split("\r\n");
             }
         };
 
         xhr.send();
 
-        text = text.split("\r\n").filter(x => x.length);
         console.log(text);
+        text = text.filter(x => x.length);
 
         const start = text.indexOf("[start]") + 1,
                 end = text.indexOf("[end]");
