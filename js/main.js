4e2ac0e7f0e35a88748777199a9f2d6ed5668872
LY02 <a0202denny@gmail.com>
2022-06-11 23:03:33 +0800

    note

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 const gameCanvas = new Game.Init();
 
-const title = 0,
-      difficult = "master",
-      ms = `${songList[title].ms}${difficult}.ms`,
-      audio = `${songList[title].audio}`;
 
-const notesInfo = Note.read(ms);
 
-const notesGroup = new THREE.Group();
 
-const notesList = Array.from(
-    {length: notesInfo.length},
-    (x, index) => new Note.Note(notesGroup, notesInfo[index])
-);
 
-gameCanvas.scene.add(notesGroup);
+function start() {
+    const title = 0,
+    difficult = "master",
+    ms = `${songList[title].ms}${difficult}.ms`,
+    audio = `${songList[title].audio}`;
 
-Game.gamePlay(gameCanvas, notesList, audio);
+    const notesInfo = Note.read(ms);
+
+    const notesGroup = new THREE.Group();
+
+    const notesList = Array.from(
+        {length: notesInfo.length},
+        (x, index) => new Note.Note(notesGroup, notesInfo[index])
+    );
+    
+    gameCanvas.scene.add(notesGroup);
+
+    Game.gamePlay(gameCanvas, notesList, audio);
+}
