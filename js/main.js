0f5486bc2df6db45b58d7b6b4a05f2a5577fa53c
LY02 <a0202denny@gmail.com>
2022-06-16 15:25:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,59 +15,118 @@ const axis = new THREE.AxesHelper(10000);
 scene.add(axis);
 
 const show = new THREE.Mesh(
-    new THREE.PlaneGeometry(500, 500), 
+    new THREE.PlaneGeometry(canvasHeight * 1.2, canvasHeight * 1.2), 
     new THREE.MeshBasicMaterial()
 );
 
 scene.add(show);
-show.position.set(canvasWidth / 2, canvasHeight * 0.3, 0);
-
+show.position.set(canvasWidth / 2, 0, 0);
 
 const songMenuList = [];
 let songIndex = 1;
+
+const domEvent = new THREEx.DomEvents(camera, renderer.domElement);
+
+function songListPlane(width, height, y, imageSize) {
+    width *= canvasWidth;
+    height *= canvasHeight
+    y *= canvasHeight;
+    imageSize *= height;
+
+    const geometryArray = [],
+          materialArray = [];
+
+    function addGeometry(geomatry, material) {
+        geometryArray.push(geomatry);
+        materialArray.push(material);
+    }
+
+    addGeometry(
+        new THREE.PlaneGeometry(width, height),
+        new THREE.MeshBasicMaterial()
+    );
+
+    const imagePlane = new THREE.PlaneGeometry(imageSize, imageSize);
+    imagePlane.translate(-(width - height) / 2, 0, 0);
+    addGeometry(
+        imagePlane,
+        new THREE.MeshBasicMaterial()
+    );
+
+    const bufferGeometry = new THREE.Mesh(
+        THREE.BufferGeometryUtils.mergeBufferGeometries(
+            geometryArray, true
+        ), 
+        materialArray
+    );
+    bufferGeometry.position.set(-canvasWidth * 0.45, y, 0);
+
+    return bufferGeometry;
+}
+
+const songListGroup = new THREE.Group();
+
+function songListPlanes() {
+    const width = 0.7,
+          height = 0.25,
+          scale = 1.2,
+          padding = 0.05,
+          imageSize = 0.8;
+
+    songListGroup.add(songListPlane(
+        width * scale, height * scale,
+        0, imageSize
+    ));
+    for (let i = 0; i < 3; i++) {
+        songListGroup.add(songListPlane(
+            width, height, 
+            padding * (i + 1) + height * (scale / 2 + 0.5 + i),
+            imageSize
+        ));
+        songListGroup.add(songListPlane(
+            width, height, 
+            -padding * (i + 1) - height * (scale / 2 + 0.5 + i),
+            imageSize
+        ));
+    }
+
+    scene.add(songListGroup);
+
+
+    for (let i = 0; i <= 6; i++) {
+        domEvent.addEventListener(songListGroup.children[i], "click", event => {
+            // if (3 == songIndex) 
+            //     return; 
+            songIndex = (i % 2 ? 
+                (songIndex - (i + 1) / 2 + songList.length - 2) : 
+                (songIndex + i / 2 - 1)
+            ) % (songList.length - 1) + 1;
+
+            changeSong();
+        });
+    }
+
+    changeSong();
+}
+songListPlanes();
+
 function changeSong() {
     const song = songList[songIndex],
     path = `${song.path}`;
     
     show.material.map = new THREE.TextureLoader().load(`${path}image.jpg`);
-    show.material.needsUpdate= true;
-    
-    for (let i in songMenuList) {
-        const plane = songMenuList[i];
-        plane.position.set(-canvasWidth / 2, (i - songIndex + 1) * -150, 0);
+    show.material.needsUpdate = true;
+
+    for (let i = 0; i <= 6; i++) {
+        const path = songList[(i % 2 ? 
+            (songIndex - (i + 1) / 2 + songList.length - 2) : 
+            (songIndex + i / 2 - 1)
+        ) % (songList.length - 1) + 1].path;
+
+        const mesh = songListGroup.children[i];
+        mesh.material[1].map = new THREE.TextureLoader().load(`${path}image.jpg`);
     }
 }
-
-const domEvent = new THREEx.DomEvents(camera, renderer.domElement);
-
-function songSel(num) {
-    const song = songList[num],
-          path = `${song.path}`;
-
-    const texture = new THREE.TextureLoader().load(`${path}image.jpg`);
-
-    const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(150, 150), 
-        new THREE.MeshBasicMaterial({
-            map: texture
-        })
-    );
-
-    domEvent.addEventListener(plane, "click", event => {
-        if (num == songIndex) 
-            return; 
-        songIndex = num;
-        changeSong();
-    });
-    
-    scene.add(plane);
-    songMenuList.push(plane);
-}
-
-for (let i = 1; i < songList.length; i++) {
-    songSel(i);
-}
-changeSong();
 
 function loop() {
     const animate = requestAnimationFrame(loop);
