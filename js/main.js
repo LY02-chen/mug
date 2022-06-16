2e1563c14cd06e5597997fe1ba4797ced5a12ffc
LY02 <a0202denny@gmail.com>
2022-06-16 11:21:52 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ scene.background = new THREE.Color(0x5d5d5d);
 const camera = new THREE.PerspectiveCamera(
     45, 16 / 9, 0.1, 10000
 );
-camera.position.set(0, canvasHeight, 1000);
-camera.lookAt(0, canvasHeight, 0);
+camera.position.set(0, 0, 1000);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(canvasWidth, canvasHeight);
@@ -20,8 +20,10 @@ const show = new THREE.Mesh(
 );
 
 scene.add(show);
-show.position.set(canvasWidth / 2, canvasHeight, 0);
+show.position.set(canvasWidth / 2, canvasHeight * 0.3, 0);
 
+
+const songMenuList = [];
 let songIndex = 1;
 function changeSong() {
     const song = songList[songIndex],
@@ -29,9 +31,12 @@ function changeSong() {
     
     show.material.map = new THREE.TextureLoader().load(`${path}image.jpg`);
     show.material.needsUpdate= true;
-
+    
+    for (let i in songMenuList) {
+        const plane = songMenuList[i];
+        plane.position.set(-canvasWidth / 2, (i - songIndex + 1) * -150, 0);
+    }
 }
-changeSong();
 
 const domEvent = new THREEx.DomEvents(camera, renderer.domElement);
 
@@ -55,13 +60,14 @@ function songSel(num) {
         changeSong();
     });
     
-    plane.position.set(-canvasWidth / 2, num * 150, 0);
     scene.add(plane);
+    songMenuList.push(plane);
 }
 
 for (let i = 1; i < songList.length; i++) {
     songSel(i);
 }
+changeSong();
 
 function loop() {
     const animate = requestAnimationFrame(loop);
